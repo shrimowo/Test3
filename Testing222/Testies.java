4165000f63f64c598fcde368a8325b66a6a59a3f
pettyem <pettyem@whs.school.nz>
2023-10-09 09:40:27 +1300

    added comments and idiot checker

---
+++ Testing222/Testies.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Testies here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Testies
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class Testies
+     */
+    public Testies()
+    {
+        // initialise instance variables
+        System.out.println("gayboy");
+    }
+}
